645bdbb58076e5b392f223835dc00afad27fa188
Arthur Koehl <avkoehl@ucdavis.edu>
2021-07-15 08:36:31 -0700

    resolves #6 and #7 and other  minor fixes; also removes unneeded fields from the index

--- R/index.R
+++ R/index.R
@@ -2,20 +2,62 @@
 # index_nfsl_data
 # index_blm_data
 # index_treesearch_data
+# index_all
 #
 # FIELDS IN AN INDEX:
-# Authors, Year, Title, DOI, LCCN, Publisher, Keywords, Description, Combined, MiscID
+# Authors, Year, Title, DOI, Publisher, Combined (Authors + Publisher), 
+# MiscID, Source
 # 
 #
+
+#' Create Combined column for authors and publishers
+#'
+#'
+#' @param authors character vector
+#' @param publisher character vector
+create_combined = function(authors, publishers) {
+    authors[authors == "NULL"] <- ""
+    publishers[publishers == "NULL"] <- ""
+    combined = paste(authors, publishers)
+    combined = trimws(combined)
+}
+
+
+#' Preprocess NFSL data
+#'
+#' @param nfsl_data dataframe of nfsl record metadata
+preprocess_nfsl_data = function(nfsl_data) {
+    # remove unneded columns and rename to match index fields
+    nfsl_data = nfsl_data[ ,c("title", "authors", "year",  
+			      "publisher", "nfsl_id") ]
+    colnames(nfsl_data) = c("Title", "Authors", "Year",
+			    "Publisher", "MiscID")
+
+    # preprocess nfsl_data
+    nfsl_data$Year = substr(nfsl_data$Year, 1,4)
+    nfsl_data$Year[nfsl_data$Year=="NULL"] <- "0"
+    nfsl_data$Year = as.numeric(nfsl_data$Year)
+    nfsl_data$Year[nfsl_data$Year>=2025] <- 0
+    nfsl_data$Year[nfsl_data$Year<=1800] <- 0
+    nfsl_data$Year = trimws(nfsl_data$Year)
+    nfsl_data$Year = as.numeric(nfsl_data$Year)
+
+    # add missing fields (Combined, DOI) make sure to not add NULL somehow
+    nfsl_data$Combined = create_combined(nfsl_data$Authors, nfsl_data$Publisher)
+    nfsl_data$DOI = NA
+    nfsl_data$Source = "NFSL"
+    return (nfsl_data)
+}
 
 #' Create index from nfsl dataset
 #'
 #'
 #' @param data optional dataframe of nfsl data
 #' @param conn optional solrclient connection object
+#' @param overwrite optional bool delete and recreate collection if exists 
 #' @export
 #' @import solrium
-index_nfsl_data = function(data = NULL, conn = NULL) {
+index_nfsl_data = function(data = NULL, conn = NULL, overwrite=FALSE) {
 
     if (is.null(data)) {
 	data = nfsl # loads from package data
@@ -26,27 +68,16 @@ index_nfsl_data = function(data = NULL, conn = NULL) {
 	conn = solrium::SolrClient$new()
     }
 
-    if (collection_exists(conn, "nfsl")) {
-	warning("'nfsl' Collection already exits, not overwriting\n")
-	return()
+    if (collection_exists(conn, "nfsl")) { 
+	if (overwrite) {
+	    collection_delete(conn, name="nfsl")
+	} else {
+	    warning("'nfsl' Collection already exits, not overwriting\n")
+	    return()
+	}
     }
 
-    # remove unneded columns and rename to match index fields
-    data = data[ ,c("title", "authors", "year", "description", "publisher", "nfsl_id") ]
-    colnames(data) = c("Title", "Authors", "Year", "Description", "Publisher", "MiscID")
-
-    # preprocess data
-    data$Year = substr(data$Year, 1,4)
-    data$Year[data$Year=="NULL"] <- "0"
-    data$Year = as.numeric(data$Year)
-    data$Year[data$Year>=2025] <- 0
-    data$Year[data$Year<=1800] <- 0
-
-    # add missing fields (Combined, Keywords, DOI, LCCN)
-    data$Combined = paste(data$Authors, data$Title, data$Publisher)
-    data$Keywords = NA
-    data$DOI = NA
-    data$LCCN = NA
+    data = preprocess_nfsl_data(data)
 
     # create collection (also called index)
     collection_create(conn, name="nfsl", numShards = 1)
@@ -57,14 +88,35 @@ index_nfsl_data = function(data = NULL, conn = NULL) {
     return()
 }
 
+#' Preprocess BLM data
+#'
+#' @param blm_data dataframe of blm record metadata
+preprocess_blm_data = function(blm_data) {
+    # remove unneded columns and rename to match index fields
+    blm_data = blm_data[ ,c("title", "authors", "year",  
+			    "publisher", "identifier") ]
+    colnames(blm_data) = c("Title", "Authors", "Year", 
+			   "Publisher", "MiscID")
+
+    blm_data$Year = substr(blm_data$Year, 0, 4)
+    blm_data$Year = as.numeric(blm_data$Year)
+
+    # add missing fields (Combined, DOI)
+    blm_data$Combined = create_combined(blm_data$Authors, blm_data$Publisher)
+    blm_data$DOI = NA
+    blm_data$Source ="BLM"
+    return(blm_data)
+}
+
 #' Create index from blm dataset
 #'
 #'
 #' @param data optional dataframe of nfsl data
 #' @param conn optional solrclient connection object
+#' @param conn overwrite bool delete and recreate collection if exists
 #' @export
 #' @import solrium
-index_blm_data = function(data = NULL, conn = NULL) {
+index_blm_data = function(data = NULL, conn = NULL, overwrite=FALSE) {
 
     if (is.null(data)) {
 	data = blm # loads from package data
@@ -75,20 +127,16 @@ index_blm_data = function(data = NULL, conn = NULL) {
 	conn = solrium::SolrClient$new()
     }
 
-    if (collection_exists(conn, "blm")) {
-	warning("'treesearch' Collection already exits, not overwriting\n")
-	return()
+    if (collection_exists(conn, "blm")) { 
+	if (overwrite) {
+	    collection_delete(conn, name="blm")
+	} else {
+	    warning("'blm' Collection already exits, not overwriting\n")
+	    return()
+	}
     }
 
-    # remove unneded columns and rename to match index fields
-    data = data[ ,c("title", "authors", "year", "topics", "publisher", "identifier", "lccn") ]
-    colnames(data) = c("Title", "Authors", "Year", "Keywords", "Publisher", "MiscID", "LCCN")
-
-    # add missing fields (Combined, Description, DOI)
-    data$Combined = paste(data$Authors, data$Title, data$Publisher)
-    data$Description = NA
-    data$DOI = NA
-
+    data = preprocess_blm_data(data)
 
     # create collection (also called index)
     collection_create(conn, name="blm", numShards = 1)
@@ -99,14 +147,34 @@ index_blm_data = function(data = NULL, conn = NULL) {
     return()
 }
 
+#' Preprocess Treesearch data
+#'
+#' @param treesearch_data dataframe of nfsl record metadata
+preprocess_treesearch_data = function(treesearch_data) {
+
+    treesearch_data = treesearch_data[ ,c("title", "authors", "year", 
+					  "source", "treesearch_id", "doi") ]
+
+    colnames(treesearch_data) = c("Title", "Authors", "Year", 
+				  "Publisher", "MiscID", "DOI")
+
+    # add missing fields (Combined)
+    treesearch_data$Combined = create_combined(treesearch_data$Authors, 
+					       treesearch_data$Publisher)
+    treesearch_data$Source = "Treesearch"
+
+    return(treesearch_data)
+}
+
 #' Create index from Treesearch dataset
 #'
 #'
 #' @param data optional dataframe of treesearch data
 #' @param conn optional solrclient connection object
+#' @param overwrite optional bool delete and recreate collection if exists
 #' @export
 #' @import solrium
-index_treesearch_data = function(data = NULL, conn = NULL) {
+index_treesearch_data = function(data = NULL, conn = NULL, overwrite=FALSE) {
 
     if (is.null(data)) {
 	data = treesearch # loads from package data
@@ -117,23 +185,74 @@ index_treesearch_data = function(data = NULL, conn = NULL) {
 	conn = solrium::SolrClient$new()
     }
 
-    if (collection_exists(conn, "treesearch")) {
-	warning("'treesearch' Collection already exits, not overwriting\n")
-	return()
+    if (collection_exists(conn, "treesearch")) { 
+	if (overwrite) {
+	    collection_delete(conn, name="treesearch")
+	} else {
+	    warning("'treesearch' Collection already exits, not overwriting\n")
+	    return()
+	}
     }
 
-    data = data[ ,c("title", "authors", "year", "keywords", "description", "source", "treesearch_id", "doi") ]
-    colnames(data) = c("Title", "Authors", "Year", "Keywords", "Description", "Publisher", "MiscID", "DOI")
-
-    # add missing fields (Combined, LCCN)
-    data$Combined = paste(data$Authors, data$Title, data$Publisher)
-    data$LCCN = NA
+    data = preprocess_treesearch_data(data)
 
     # create collection (also called index)
     collection_create(conn, name="treesearch", numShards = 1)
 
     # add documents
     conn$add(data, "treesearch")
+
+    return()
+}
+
+#' Create collection containing nfsl, blm and treesearch datasets
+#'
+#' @param nfsl optional dataframe of nfsl record metadata
+#' @param blm optional dataframe of blm record metadata
+#' @param treesearch optional dataframe of treesearch record metadata
+#' @param conn optional solrclient connection object
+#' @param overwrite optional bool delete and recreate collection if exists
+#' @export 
+#' @import solrium
+index_all = function(nfsl_data= NULL, blm_data=NULL, treesearch_data=NULL,
+		     conn=NULL, overwrite=FALSE) {
+
+    if (is.null(nfsl_data)) {
+	nfsl_data = nfsl # loads from package data
+    }
+
+    if (is.null(blm_data)) {
+	blm_data = blm # loads from package data
+    }
+
+    if (is.null(treesearch_data)) {
+	treesearch_data = treesearch # loads from package data
+    }
+
+    if (is.null(conn)) {
+	# use the default connection info
+	conn = solrium::SolrClient$new()
+    }
+
+    if (collection_exists(conn, "all")) { 
+	if (overwrite) {
+	    collection_delete(conn, name="all")
+	} else {
+	    warning("'all' Collection already exits, not overwriting\n")
+	    return()
+	}
+    }
+
+    nfsl_data = preprocess_nfsl_data(nfsl_data)
+    blm_data = preprocess_blm_data(blm_data)
+    treesearch_data = preprocess_treesearch_data(treesearch_data)
+    all_data = rbind(nfsl_data, blm_data, treesearch_data)
+
+    # create collection (also called index)
+    collection_create(conn, name="all", numShards = 1)
+
+    # add documents
+    conn$add(all_data, "all")
 
     return()
 }
